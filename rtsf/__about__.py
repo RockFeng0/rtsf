d72750c97b3d67f1ec0287969542dfd659084957
rockfeng0 <lkf20031988@163.com>
2022-06-29 11:03:17 +0800

    update to v2.9.5, fix unzip issue and update FileUtils

--- rtsf/__about__.py
+++ rtsf/__about__.py
@@ -3,7 +3,7 @@
 
 __title__ = "rtsf"
 __short_desc__ = "rock4 test service framework"
-__version__ = "2.9.4"
+__version__ = "2.9.5"
 __autor__ = u"罗科峰"
 __author_email__ = "lkf20031988@163.com"
 __license__ = 'MIT'
