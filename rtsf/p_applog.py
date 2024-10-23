784ecfcf74ff5bcc77f2f7365e8a4ade7a30cddf
luokefengds <luokefengds@chinamobile.com>
2024-10-23 16:01:41 +0800

    refactor: 日志格式和级别调整

--- rtsf/p_applog.py
+++ rtsf/p_applog.py
@@ -25,17 +25,16 @@ class AppLog(object):
     """ record the logs with your preference  """
     def __init__(self, logger_name=None):
         self.logger = logging.getLogger(logger_name)
-        self.log_colors = {}        
-        self.formatter = logging.Formatter(u'#%(asctime)s %(levelname)-8s: %(message)s')
-    
-    @staticmethod
-    def setup_logger(log_level, log_file=None, logger_name=None):
+        self.has_color = False
+        self.formatter = logging.Formatter(u'%(asctime)s %(levelname)-8s: %(message)s')
+
+    def setup_logger(self, log_level, log_file=None, has_color=False):
         """setup logger
             @param log_level: debug/info/warning/error/critical
             @param log_file: log file path
-            @param logger_name: the name of logger, default is 'root' if not specify
+            @param has_color:
         """
-        app_logger = AppLog(logger_name)
+        self.has_color = has_color
         level = getattr(logging, log_level.upper(), None)
         if not level:
             color_print("Invalid log level: %s" % log_level, "RED")
@@ -46,36 +45,35 @@ class AppLog(object):
             sys.tracebacklimit = 0
     
         if log_file:
-            app_logger._handle2file(log_file)
-        else:
-            app_logger._handle2screen(color=True)
+            self._handle2file(log_file)
+        self._handle2screen()
         
-        app_logger.logger.setLevel(level)
+        self.logger.setLevel(level)
     
     @property
-    def log_debug(self):
+    def debug(self):
         return self._tolog("debug")
     
     @property
-    def log_info(self):
+    def info(self):
         return self._tolog("info")
     
     @property
-    def log_warning(self):
+    def warning(self):
         return self._tolog("warning")
     
     @property
-    def log_error(self):
+    def error(self):
         return self._tolog("error")
     
     @property    
-    def log_critical(self):
+    def critical(self):
         return self._tolog("critical")
     
     def _tolog(self, level):
         """ log with different level """
         def wrapper(msg):
-            if self.log_colors:
+            if self.has_color:
                 color = self.log_colors[level.upper()]
                 getattr(self.logger, level.lower())(coloring("- {}".format(msg), color))
             else:
@@ -83,18 +81,19 @@ class AppLog(object):
     
         return wrapper
                
-    def _handle2file(self,file_path):
+    def _handle2file(self, file_path):
         if os.path.isdir(os.path.abspath(os.path.dirname(file_path))):
-            fh = logging.FileHandler(file_path, mode='a')          
+            fh = logging.FileHandler(file_path, mode='a')
+            fh.setLevel(logging.DEBUG)
             fh.setFormatter(self.formatter)    
             self.logger.addHandler(fh)
         else:
             raise p_exception.DirectoryNotFound(file_path)
     
-    def _handle2screen(self, color=False):
+    def _handle2screen(self):
         ch = logging.StreamHandler()
-        
-        if color:
+        ch.setLevel(logging.INFO)
+        if self.has_color:
             self.log_colors = {
                 'DEBUG':    'cyan',
                 'INFO':     'green',
