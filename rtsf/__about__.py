4920621b98304fd35d7f03f12b91199a7d198cca
luokefengds <luokefengds@chinamobile.com>
2024-10-29 09:51:13 +0800

    update version to v2.9.8

--- rtsf/__about__.py
+++ rtsf/__about__.py
@@ -3,7 +3,7 @@
 
 __title__ = "rtsf"
 __short_desc__ = "a test service framework"
-__version__ = "2.9.7"
+__version__ = "2.9.8"
 __autor__ = u"RockFeng0"
 __author_email__ = "281589463@qq.com"
 __license__ = 'MIT'
