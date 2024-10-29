ef084bbc5c9a2350bfdca5a3503fc9e3a02b85f3
luokefengds <luokefengds@chinamobile.com>
2024-10-29 09:50:38 +0800

    refactor: 简化封装logging 模块的日志和使用方式

--- rtsf/p_applog.py
+++ rtsf/p_applog.py
@@ -23,96 +23,108 @@ def color_print(msg, color="WHITE"):
 
 class AppLog(object):
     """ record the logs with your preference  """
-    def __init__(self, logger_name=None):
-        self.logger = logging.getLogger(logger_name)
-        self.has_color = False
-        self.formatter = logging.Formatter(u'%(asctime)s %(levelname)-8s: %(message)s')
-
-    def setup_logger(self, log_level, log_file=None, has_color=False):
-        """setup logger
-            @param log_level: debug/info/warning/error/critical
-            @param log_file: log file path
-            @param has_color:
-        """
+    def __init__(self, logger_name=None, log_file=None, fmt=None, log_level=logging.DEBUG, has_color=False):
+        self._logger = logging.getLogger(logger_name)
         self.has_color = has_color
-        level = getattr(logging, log_level.upper(), None)
-        if not level:
-            color_print("Invalid log level: %s" % log_level, "RED")
-            sys.exit(1)
-    
+
+        self.fmt = fmt if fmt else u"%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)-8s: %(message)s"
+        # self.formatter = logging.Formatter(u'%(asctime)s %(levelname)-8s: %(message)s')
+        self._format = logging.Formatter(self.fmt)
+
         # hide traceback when log level is INFO/WARNING/ERROR/CRITICAL
-        if level >= logging.INFO:
+        if log_level >= logging.INFO:
             sys.tracebacklimit = 0
-    
+
+        # 默认打印console日志
         if log_file:
             self._handle2file(log_file)
         self._handle2screen()
-        
-        self.logger.setLevel(level)
-    
-    @property
-    def debug(self):
-        return self._tolog("debug")
-    
-    @property
-    def info(self):
-        return self._tolog("info")
-    
-    @property
-    def warning(self):
-        return self._tolog("warning")
-    
-    @property
-    def error(self):
-        return self._tolog("error")
-    
-    @property    
-    def critical(self):
-        return self._tolog("critical")
-    
-    def _tolog(self, level):
-        """ log with different level """
-        def wrapper(msg):
-            if self.has_color:
-                color = self.log_colors[level.upper()]
-                getattr(self.logger, level.lower())(coloring("- {}".format(msg), color))
-            else:
-                getattr(self.logger, level.lower())(msg)
-    
-        return wrapper
+        self._logger.setLevel(log_level)
+
+    def get_logger(self):
+        return self._logger
+
+    # @property
+    # def debug(self):
+    #     return self._tolog("debug")
+    #
+    # @property
+    # def info(self):
+    #     return self._tolog("info")
+    #
+    # @property
+    # def warning(self):
+    #     return self._tolog("warning")
+    #
+    # @property
+    # def error(self):
+    #     return self._tolog("error")
+    #
+    # @property
+    # def critical(self):
+    #     return self._tolog("critical")
+    #
+    # def _tolog(self, level):
+    #     """ log with different level """
+    #     def wrapper(msg):
+    #         if self.has_color:
+    #             color = self.log_colors[level.upper()]
+    #             getattr(self._logger, level.lower())(coloring("- {}".format(msg), color))
+    #         else:
+    #             getattr(self._logger, level.lower())(msg)
+    #
+    #     return wrapper
                
     def _handle2file(self, file_path):
+        """ 文件 记录Debug级别日志
+        :return:
+        """
         if os.path.isdir(os.path.abspath(os.path.dirname(file_path))):
             fh = logging.FileHandler(file_path, mode='a')
             fh.setLevel(logging.DEBUG)
-            fh.setFormatter(self.formatter)    
-            self.logger.addHandler(fh)
+            fh.setFormatter(self._format)
+            self._logger.addHandler(fh)
         else:
             raise p_exception.DirectoryNotFound(file_path)
     
     def _handle2screen(self):
+        """ console 打印Info级别日志
+        :return:
+        """
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
-        if self.has_color:
-            self.log_colors = {
-                'DEBUG':    'cyan',
-                'INFO':     'green',
-                'WARNING':  'yellow',
-                'ERROR':    'red',
-                'CRITICAL': 'red',
-            }
-               
-            color_formatter = ColoredFormatter(u"%(log_color)s%(bg_white)s#%(asctime)s %(levelname)-8s%(reset)s %(message)s",
-                datefmt=None,
-                reset=True,
-                log_colors=self.log_colors
-            )   
-            
-            ch.setFormatter(color_formatter)
-        else:
-            ch.setFormatter(self.formatter)
-            
-        self.logger.addHandler(ch)
+        # if self.has_color:
+        #     self.log_colors = {
+        #         'DEBUG':    'cyan',
+        #         'INFO':     'green',
+        #         'WARNING':  'yellow',
+        #         'ERROR':    'red',
+        #         'CRITICAL': 'red',
+        #     }
+        #
+        #     color_formatter = ColoredFormatter(u"%(log_color)s%(bg_white)s#" + self.fmt,
+        #         datefmt=None,
+        #         reset=True,
+        #         log_colors=self.log_colors
+        #     )
+        #
+        #     ch.setFormatter(color_formatter)
+        # else:
+        #     ch.setFormatter(self._format)
+        ch.setFormatter(self._format)
+        self._logger.addHandler(ch)
 
 
-logger = AppLog()
+if __name__ == '__main__':
+    console = AppLog(logger_name="TestApp1").get_logger()
+    console.debug("console debug")
+    console.info("console info")
+    console.warning("console warning")
+    console.error("console error")
+
+    logger = AppLog(logger_name="TestApp2", log_file=r"D:\auto\buffer\test_sdfsdf_20240403.log").get_logger()
+    logger.debug("logger debug")
+    logger.info("logger info")
+    logger.warning("logger warning")
+    logger.error("logger error")
+
